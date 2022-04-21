c1f940cd9368223204d644ed990320a682c7d110
artyommatt <artyommatyushonok@gmail.com>
2022-04-21 20:05:27 +0300

    ADD: base and main pages, UPD: add method to test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,14 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
+from selenium.webdriver.common.by import By
+
+
+link: str = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser) -> None:
+    login_link = browser.find_element(by=By.CSS_SELECTOR, value="#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser) -> None:
+    browser.get(link)
+    go_to_login_page(browser)
