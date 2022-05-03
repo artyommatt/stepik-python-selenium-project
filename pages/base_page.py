ccfe06ea8bfde4035784a2618fdfb3a743a5dff3
artyommatt <artyommatyushonok@gmail.com>
2022-05-03 17:45:20 +0300

    UPD: added NoSuchElementException handler

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,23 +1,18 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-import pytest
 
 
 class BasePage:
-    def __int__(self, browser, url):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
 
-    # def go_to_login_page(self):
-    #     login_link = self.browser.find_element(by=By.CSS_SELECTOR, value="#login_link")
-    #     login_link.click()
-    #
-    # def is_element_present(self, how, what):
-    #     try:
-    #         self.browser.find_element(how, what)
-    #     except NoSuchElementException:
-    #         return False
-    #     return True
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -3,9 +3,9 @@ from .base_page import BasePage
 
 
 class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
-
     def go_to_login_page(self):
         login_link = self.browser.find_element(by=By.CSS_SELECTOR, value="#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
