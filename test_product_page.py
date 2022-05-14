3da435a73b6f274c327a322a0cd70e988f7d01c3
artyommatt <artyommatyushonok@gmail.com>
2022-05-14 15:20:45 +0300

    UPD: add class to login page test on main page

--- test_product_page.py
+++ test_product_page.py
@@ -65,8 +65,9 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
-    page = BasketPage(browser, link)
+    page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
-    page.should_empty_basket()
-    page.should_be_empty_basket_message()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_empty_basket()
+    basket_page.should_be_empty_basket_message()
